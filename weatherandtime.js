7e5fa39702c30a0d788e7a66006b1ae9514f6fe9
Fradniev Gonzalez <fradniev@hotmail.com>
2019-12-03 22:11:47 -0300

    Showing date and temperature of cities

--- weatherandtime.js
+++ weatherandtime.js
@@ -1,6 +1,7 @@
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const utf8 = require("utf8");
-const apiKey = "0a66000ee16210d9cffcde19fa27a62a";
+const apiKeyWeather = "0a66000ee16210d9cffcde19fa27a62a";
+const apiKeyDate = "67baa1d6fec3468caed99152addfeb05";
 const cityNames = [
 	"./weather New York",
 	"10005",
@@ -9,13 +10,13 @@ const cityNames = [
 	"Pluto",
 ];
 
-function sendRequest(cityName) {
+function requestWeather(cityName) {
 	let request = new XMLHttpRequest();
-	const apiLink = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=${apiKey}`;
+	const apiLink = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=${apiKeyWeather}`;
 	request.open("GET", apiLink);
 	request.onload = () => {
 		try {
-			manageRequest(request, cityName);
+			manageRequestWeather(request, cityName);
 		} catch(error) {
 			console.log(error);
 		}
@@ -23,19 +24,41 @@ function sendRequest(cityName) {
 	request.send();
 }
 
-function manageRequest(request, cityName) {
+function manageRequestWeather(request, cityName) {
 	let weatherData = JSON.parse(request.responseText);
 	if(request.status >= 200 && request.status < 400) {
 		let temperature = weatherData.main.temp;
-		console.log(`The temperature of ${utf8.decode(cityName)} is ${temperature}`)
+		console.log(`The temperature of ${decodeURIComponent(cityName)} is ${temperature}`)
 	} else {
-		console.log(`${cityName} doesn't exist! Kindly check`)
+		console.log(`${decodeURIComponent(cityName)} doesn't exist! Kindly check`)
 	}
+}
+
+function requestDate(cityName) {
+	let request = new XMLHttpRequest();
+	const apiLink = `https://api.opencagedata.com/geocode/v1/json?q=${cityName}&key=${apiKeyDate}`;
+	request.open("GET", apiLink);
+	request.onload = () => {
+		try {
+			manageRequestDate(request, cityName);
+		} catch(error) {
+			console.log(`${decodeURIComponent(cityName)} doesn't exist! Kindly check`)
+		}
+	}
+	request.send();
+}
+
+function manageRequestDate(request, cityName) {
+	let dateData = JSON.parse(request.responseText);
+	let timezone = dateData.results[0].annotations.timezone.offset_sec
+	let date = new Date( new Date().getTime() + timezone*1000).toUTCString();
+	console.log(`The date of ${decodeURIComponent(cityName)} is ${date}`)
 }
 
 function start() {
 	for (var i = 0; i < cityNames.length; i++) {
-		sendRequest(utf8.encode(cityNames[i]))
+		requestWeather(encodeURIComponent(cityNames[i]))
+		requestDate(encodeURIComponent(cityNames[i]))
 	}
 }
 
